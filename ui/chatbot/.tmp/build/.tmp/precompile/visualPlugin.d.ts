040a07f941af9e9a6dd49e011b41f929aff97bfd
Guilherme-Alvees <guilherme.alvs.br@gmail.com>
2025-04-03 10:40:47 -0300

    add ui

---
+++ ui/chatbot/.tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -0,0 +1,4 @@
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+declare var chatbot5D9D82DEE4964BBE9CB1A8945935DADA_DEBUG: IVisualPlugin;
+export default chatbot5D9D82DEE4964BBE9CB1A8945935DADA_DEBUG;
